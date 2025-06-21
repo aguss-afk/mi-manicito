67ec70b58897b96b75f8c1182d74a69621183752
Agustin Arnez <74320809+aguss-afk@users.noreply.github.com>
2025-06-21 19:37:05 -0400

    se viene

--- js/script.js
+++ js/script.js
@@ -1,16 +1,4 @@
-
-fetch('header.html')
-  	.then(res => res.text())
-    .then(data => {
-    	document.getElementById('navbar').innerHTML = data;
-    });
-window.addEventListener('scroll', function() {
-    const header = document.getElementById('nav');
-    const scrollPosition = window.scrollY;
-
-    if (scrollPosition > 10) {
-        header.classList.add('transparent');
-    } else {
-        header.classList.remove('transparent');
-    }
-});
+var preload = document.querySelector(".preloader");
+window.addEventListener('load', function(){
+    preload.style.display = 'none';
+})
