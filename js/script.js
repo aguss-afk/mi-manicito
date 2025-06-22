b3e80e9402fa9a3bd97966fb9b063c21dd65171e
Agustin Arnez <74320809+aguss-afk@users.noreply.github.com>
2025-06-21 20:35:59 -0400

    Ya se vino

--- js/script.js
+++ js/script.js
@@ -1,4 +1,10 @@
-var preload = document.querySelector(".preloader");
-window.addEventListener('load', function(){
-    preload.style.display = 'none';
-})
+var preload = document.getElementById("preloader");
+window.addEventListener('load', function() {
+  setTimeout(() => {
+    preload.classList.add('hidden'); 
+
+    setTimeout(() => {
+      preload.style.display = 'none';
+    }, 2000);
+  }, 1000);
+});
